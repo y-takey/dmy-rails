9e50f6eb51e5a42fd42b0e434269a44133e441b0
Yuichiro Take <y.takey@gmail.com>
2017-08-24 12:52:03 +0900

    commit1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:code, :name, :price)
+      params.require(:book).permit(:code, :name, :price, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822142437) do
+ActiveRecord::Schema.define(version: 20170824000000) do
 
   create_table "books", force: :cascade do |t|
     t.string "code"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170822142437) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
